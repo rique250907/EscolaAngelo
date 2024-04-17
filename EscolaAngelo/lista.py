be18aaf6dc796dd65bdfcb3f89738ca379af7405
henrique guedes <166624270+rique250907@users.noreply.github.com>
2024-04-17 08:28:02 -0300

    Add files via upload

---
+++ EscolaAngelo/lista.py
@@ -0,0 +1,11 @@
+listanomesd = []
+
+listasnomes.apend("Edson")
+listasnomes.append("Tamires")
+listasnomes.append("sofia")
+
+print (listanomas([1]))
+
+for nome in listanomes:
+    print(nome)
+    
